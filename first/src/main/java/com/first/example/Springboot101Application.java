eef50d5dea2af993cf564d5d962a591d72d53864
usb777 <dzsamoila@gmail.com>
2023-07-23 14:40:44 -0400

    Project 101

---
+++ first/src/main/java/com/first/example/Springboot101Application.java
@@ -0,0 +1,13 @@
+package com.first.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot101Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot101Application.class, args);
+	}
+
+}
