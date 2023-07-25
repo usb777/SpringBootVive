02debdb3425aa1fcc4bdd7e8fc6c03d0232c52bb
usb777 <dzsamoila@gmail.com>
2023-07-25 00:11:55 -0400

    project 203 with Model

--- project203/src/main/java/com/song/project203/controller/HomeController.java
+++ project203/src/main/java/com/song/project203/controller/HomeController.java
@@ -2,30 +2,30 @@ package com.song.project203.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import com.song.project203.model.Song;
 
 @Controller
 public class HomeController {
 	
-	@RequestMapping("/")
-	public String homePage() {
-		return "index";
-	}
 	
-	@RequestMapping("/loadform")
-	public String loadFormPage()
+	@GetMapping("/songform")
+	public String loadFormPage(Model model)
 	{
-		return "formtemplate";
+		model.addAttribute("song", new Song());
+		return "songform";
 	}
 	
-	@RequestMapping("/processform")
-	public String loadFromPage(@RequestParam("login") String login, Model model)
+	@PostMapping("/songform")
+	public String loadFromPage(@ModelAttribute Song song, Model model)
 	{
-		model.addAttribute("loginval", login);
-		return "confirm";
+		model.addAttribute("song", song);
+		return "confirmsong";
 	}
-	
-	
 
 }
