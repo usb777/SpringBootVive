24ebf1315570d06e020726aa796efd2d543b3c46
MrDzen <dzsamoila@gmail.com>
2023-08-22 23:59:28 -0400

    Stream api

--- Stream_Api/src/main/java/org/streamapi/Main.java
+++ Stream_Api/src/main/java/org/streamapi/Main.java
@@ -1,6 +1,9 @@
 package org.streamapi;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Main {
@@ -18,6 +21,15 @@ public class Main {
         System.out.println(anyElement.toString());
 
     }
+
+    protected static void collectDemo()
+    {
+        List number = Arrays.asList(2,3,4,5);
+        List square = number.stream().map(x->multiply(x)).collect(Collectors.toList());
+
+    }
+     private static int multiply(Integer a)
+     {return a*a;}
 
 
 }
