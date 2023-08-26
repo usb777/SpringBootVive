4e9b6f3da50bc4fbf694c0c99a31cd465bb5f809
MrDzen <dzsamoila@gmail.com>
2023-08-26 16:38:49 -0400

    application started

--- Kafka/json_deserialization/src/main/java/com/kafka/json_deserialization/JsonDeserializationApplication.java
+++ Kafka/json_deserialization/src/main/java/com/kafka/json_deserialization/JsonDeserializationApplication.java
@@ -1,13 +1,17 @@
 package com.kafka.json_deserialization;
 
+import com.kafka.json_deserialization.producer.KafkaProducer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JsonDeserializationApplication {
-
+	private static final Logger log = LoggerFactory.getLogger(KafkaProducer.class);
 	public static void main(String[] args) {
 		SpringApplication.run(JsonDeserializationApplication.class, args);
+		log.info("Application started");
 	}
 
 }
