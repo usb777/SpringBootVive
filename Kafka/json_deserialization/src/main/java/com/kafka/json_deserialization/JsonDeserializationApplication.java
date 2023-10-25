724a65cfe9fce26d3c296d8cbfcf154f718419e3
MrDzen <dzsamoila@gmail.com>
2023-10-24 22:30:56 -0400

    Course Tutorialspoint

--- Kafka/json_deserialization/src/main/java/com/kafka/json_deserialization/JsonDeserializationApplication.java
+++ Kafka/json_deserialization/src/main/java/com/kafka/json_deserialization/JsonDeserializationApplication.java
@@ -1,13 +1,20 @@
 package com.kafka.json_deserialization;
 
 import com.kafka.json_deserialization.producer.KafkaProducer;
+import lombok.Data;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.sql.DataSourceDefinition;
+
 @SpringBootApplication
+@Data
 public class JsonDeserializationApplication {
+
+
+
 	private static final Logger log = LoggerFactory.getLogger(KafkaProducer.class);
 	public static void main(String[] args) {
 		SpringApplication.run(JsonDeserializationApplication.class, args);
