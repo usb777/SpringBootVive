38dde2e54a10c88ef30b85b274fb1da70b5f44be
MrDzen <dzsamoila@gmail.com>
2023-11-05 23:55:47 -0500

    Code changes

--- CourseT/server-product-management/src/main/java/com/sha/serverproductmanagement/ServerProductManagementApplication.java
+++ CourseT/server-product-management/src/main/java/com/sha/serverproductmanagement/ServerProductManagementApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ServerProductManagementApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(ServerProductManagementApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(ServerProductManagementApplication.class, args);
+    }
 }
