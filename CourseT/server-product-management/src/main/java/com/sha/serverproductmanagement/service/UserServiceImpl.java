a3f0fc9035195453d91aaab1bee6503da8a01e40
MrDzen <dzsamoila@gmail.com>
2023-11-07 17:06:50 -0500

    Controller

--- CourseT/server-product-management/src/main/java/com/sha/serverproductmanagement/service/UserServiceImpl.java
+++ CourseT/server-product-management/src/main/java/com/sha/serverproductmanagement/service/UserServiceImpl.java
@@ -3,7 +3,7 @@ package com.sha.serverproductmanagement.service;
 import com.sha.serverproductmanagement.model.User;
 import com.sha.serverproductmanagement.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
