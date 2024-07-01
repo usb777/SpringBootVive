ce423f1dac11b91e956d29195bc7e8dabadec033
MrDzen <dzsamoila@gmail.com>
2024-06-30 23:59:14 -0400

    develop Basic project fro SpringBoot

--- Basic/webproject/src/main/java/com/basic/webproject/WebprojectApplication.java
+++ Basic/webproject/src/main/java/com/basic/webproject/WebprojectApplication.java
@@ -2,8 +2,12 @@ package com.basic.webproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories("com.basic.webproject.repo")
+@EntityScan("com.basic.webproject.model")
 public class WebprojectApplication {
 
 	public static void main(String[] args) {
---
+++ Basic/webproject/src/main/java/com/basic/webproject/repo/BookRepository.java
@@ -0,0 +1,10 @@
+package com.basic.webproject.repo;
+
+import com.basic.webproject.model.Book;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface BookRepository extends CrudRepository<Book, Long> {
+    List<Book> findByTitle(String title);
+}
